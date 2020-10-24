42afdc5122b0f5c5f30d95a2e4c5c266157ea2c7
dmvoro <dmvoro@gmail.com>
2020-10-24 14:25:59 +0300

    feat: towel sort fix

--- src/index.js
+++ src/index.js
@@ -7,5 +7,8 @@ module.exports = function towelSort (matrix) {
     matrix.map((element,index) => {if(index%2!=0)element.reverse()})
     return matrix.flat()
     }
+    else{
+      return []
+    }
   
 }
